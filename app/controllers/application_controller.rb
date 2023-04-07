a1bcb7ac0d46937e3c872ded1de349428cf70e3a
Athena Saldanha <asaldanha@chicagobooth.edu>
2023-04-07 15:08:04 +0000

    square

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
 def blank_square_form
 render({:template=>"calculation_templates/square_form.html.erb"})
 end
+
+def calculate_square
+  #params ={"elephant=>"42"}
+  @nums = params.fetch("elephant").to_i
+  @square_of_num=@num**2
+  render({:template=>"calculation_templates/square_results.html.erb"})
+  end
 end
